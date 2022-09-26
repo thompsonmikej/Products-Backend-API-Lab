19293fe9399e39387ad8bc3908bce1a69812c268
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-09-26 11:28:51 -0400

    Superuser admin created

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
-class Products(models.Model):
+class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
--- products/views.py
+++ products/views.py
@@ -1,8 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
 
 @api_view(['GET'])
 def products_list(request):
+    products = Product.objects.all()
 
-    return Response('ok')
+    serializer = ProductSerializer(products, many=True)
+
+    return Response(serializer.data)
 
